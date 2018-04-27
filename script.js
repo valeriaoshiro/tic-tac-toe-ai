cb2dc680862de85fae18e9e2dc00b5b6b89a8bcc
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-04-27 22:27:47 +0000

    Start changing value when square is clicked

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
+	let isCompTurn = true;
 	$('.squares').click(function(e){
-		console.log(e);
+		if(isCompTurn){
+			e.target.innerHTML = "X";
+		} else {
+			e.target.innerHTML = e.target.id;
+		}
+		
 	})
+
+	let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+
 })
