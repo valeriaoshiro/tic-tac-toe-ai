30e3506487ac67ebd4db04c7aada7fdbfcf4cb90
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-04-30 16:45:17 +0000

    Create winning combinations, cleaned code in click event

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 $(document).ready(function(){
 	let isCompTurn = true;
+	let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
 	$('.squares').click(function(e){
 		if(isCompTurn){
 			e.target.innerHTML = "X";
-			isCompTurn = !isCompTurn;
-			$(this).off('click');
+			board[e.target.id] = "X";
 		} else {
 			e.target.innerHTML = "O";
-			isCompTurn = !isCompTurn;
-			$(this).off('click');
+			board[e.target.id] = "O";
 		}
-		
+		$(this).off('click');
+		isCompTurn = !isCompTurn;
+
+		if(winningComb()){
+			$('.squares').off('click');
+			console.log('won');
+		}
 	})
 
-	let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
+	var winningComb = () => {
+		if(board[0] === board[1] && board[0] === board[2] ||
+		   board[3] === board[4] && board[3] === board[5] ||
+		   board[6] === board[7] && board[6] === board[8] || 
+		   board[0] === board[3] && board[0] === board[6] ||
+		   board[1] === board[4] && board[1] === board[7] ||
+		   board[2] === board[5] && board[2] === board[8] ||
+		   board[0] === board[4] && board[0] === board[8] ||
+		   board[2] === board[4] && board[2] === board[6] ){
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 })
