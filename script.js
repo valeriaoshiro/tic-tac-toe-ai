5e25e32407d6dd9dbf78f68fe7ee74b4b8777a87
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-04-30 17:26:39 +0000

    Add h1 for message, also created tie option

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 $(document).ready(function(){
 	let isCompTurn = true;
+	let turnCount = 0;
 	let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 	$('.squares').click(function(e){
+		turnCount++;
+
 		if(isCompTurn){
-			e.target.innerHTML = "X";
-			board[e.target.id] = "X";
+			e.target.innerHTML = 'X';
+			board[e.target.id] = 'X';
 		} else {
-			e.target.innerHTML = "O";
-			board[e.target.id] = "O";
+			e.target.innerHTML = 'O';
+			board[e.target.id] = 'O';
 		}
-		$(this).off('click');
-		isCompTurn = !isCompTurn;
 
 		if(winningComb()){
 			$('.squares').off('click');
-			console.log('won');
+			isCompTurn ? $('h1').html('You lost') : $('h1').html('You won');
+		} else if(!winningComb() && turnCount === 9){
+			$('h1').html("It's a tie");
 		}
+
+		$(this).off('click');
+		isCompTurn = !isCompTurn;
 	})
 
 	var winningComb = () => {
