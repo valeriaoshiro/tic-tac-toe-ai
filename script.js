c8fc1ba9de33ffc09f0ea9de0942fe957d5eda82
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-05-04 20:42:04 +0000

    AI is working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	let isCompTurn, board;
 
 	let init = () => {
-		isCompTurn = false;
+		turn = 'X';
 		board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 		console.log('initial board', board);
 		$('.squares').html('');
@@ -10,97 +10,118 @@ $(document).ready(function(){
 		$('.squares').off('click');
 
 		$('.squares').on('click', function(e){
-			if(!isCompTurn){
-				e.target.innerHTML = 'X';
-				board[e.target.id] = 'X';
+			if(turn === 'X'){
+				e.target.innerHTML = turn;
+				board[e.target.id] = turn;
 				$(this).off('click');
 			}
-			console.log('user ', board)
-			if(!isItDone()) compTurn();
+			console.log('user', board);
+			if(!isItDone(board, turn)) compTurn();
 		});
 	}
 
 	let compTurn = () => {
-		let spot = minimax(board, isCompTurn); //temporary
+		let spot = minimax(board, turn).index;
 		$('.squares')[spot].innerHTML = 'O';
 		board[spot] = 'O';
-		console.log(minimax(board, 0, isCompTurn));
 		$(`.squares`).off('click', `#${spot}`);
-		isItDone();
-		console.log('comp', board)
+		console.log('comp', board);
+		isItDone(board, turn);
 	}	
 
-	let isItDone = () => {
-		if(winningComb()){
+	let isItDone = (newBoard, newTurn) => {
+		if(winningComb(newBoard, newTurn)){
 			$('.squares').off('click');
-			isCompTurn ? $('h1').html('You lost') : $('h1').html('You won');
+			if(newTurn === 'X'){
+				$('h1').html('You won');
+			} else if (turn === 'O'){
+				$('h1').html('You lost');
+			}
 			return true;
-		} else if(!winningComb() && availableSpots(board).length === 0){
+		} else if(!winningComb(newBoard, newTurn) && availableSpots(newBoard).length === 0){
 			$('h1').html("It's a tie");
 			return true;
 		}
-		isCompTurn = !isCompTurn;
+		if(turn === 'X'){
+			turn = 'O';
+		} else if (turn === 'O'){
+			turn = 'X';
+		}
 		return false;
 	}
 
-	let winningComb = () => {
-		if(board[0] === board[1] && board[0] === board[2] ||
-		   board[3] === board[4] && board[3] === board[5] ||
-		   board[6] === board[7] && board[6] === board[8] || 
-		   board[0] === board[3] && board[0] === board[6] ||
-		   board[1] === board[4] && board[1] === board[7] ||
-		   board[2] === board[5] && board[2] === board[8] ||
-		   board[0] === board[4] && board[0] === board[8] ||
-		   board[2] === board[4] && board[2] === board[6] ){
+	let winningComb = (newBoard, newTurn) => {
+		if(newBoard[0] === newBoard[1] && newBoard[0] === newBoard[2] && newBoard[0] === newTurn ||
+		   newBoard[3] === newBoard[4] && newBoard[3] === newBoard[5] && newBoard[3] === newTurn ||
+		   newBoard[6] === newBoard[7] && newBoard[6] === newBoard[8] && newBoard[6] === newTurn || 
+		   newBoard[0] === newBoard[3] && newBoard[0] === newBoard[6] && newBoard[0] === newTurn ||
+		   newBoard[1] === newBoard[4] && newBoard[1] === newBoard[7] && newBoard[1] === newTurn ||
+		   newBoard[2] === newBoard[5] && newBoard[2] === newBoard[8] && newBoard[2] === newTurn ||
+		   newBoard[0] === newBoard[4] && newBoard[0] === newBoard[8] && newBoard[0] === newTurn ||
+		   newBoard[2] === newBoard[4] && newBoard[2] === newBoard[6] && newBoard[2] === newTurn){
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	let availableSpots = (board) => {
-		return board.filter(i => i != 'X' && i != 'O');
+	let availableSpots = (newBoard) => {
+		return newBoard.filter(i => i != 'X' && i != 'O');
 	}
 
-	let minimax = (board, isComp) => {
-		let availSpots = availableSpots(board);
+	let minimax = (reboard, player) => {	
+   		let array = availableSpots(reboard);
+  		if (winningComb(reboard, 'X')) {
+    		return {
+      			score: -10
+    		};
+  		} else if (winningComb(reboard, 'O')) {
+    		return {
+      			score: 10
+    		};
+  		} else if (array.length === 0) {
+    		return {
+      			score: 0
+    		};
+  		}
 
-		if(winningComb() && isComp){
-			return {score: 1};
-		} else if(winningComb() && !isComp){
-			return {score: -1};
-		} else if(availSpots.length === 0){
-			return {score: 0};
-		}
+  		var moves = [];
+  		for (var i = 0; i < array.length; i++) {
+    		var move = {};
+    		move.index = reboard[array[i]];
+    		reboard[array[i]] = player; 
 
-		var moves = [];
+    		if(player == 'O'){
+    			var g = minimax(reboard, 'X');
+      			move.score = g.score;
+    		} else {
+    			var g = minimax(reboard, 'O');
+      			move.score = g.score;
+    		}
+    		reboard[array[i]] = move.index;
+    		moves.push(move);
+  		}
 
-		availSpots.forEach(spot => {
-			let move = {};
-			move.index = spot;
-			board[spot] = isComp;
-			let result = minimax(board, !isComp);
-			move.score = result.score;
-			board[spot] =  move.index;
-			moves.push(move);
-		});
-
-		let bestMove;
-
-		if(isComp){
-			let bestValue = -1000;
-			moves.forEach((move, i) => {
-				bestValue = Math.max(move.score, bestValue);
-				bestMove = i;
-			});
-		} else {
-			let bestValue = 1000;
-			moves.forEach((move, i) => {
-				bestValue = Math.min(move.score, bestValue);
-				bestMove = i
-			});
-		}
-		return moves[bestMove];
+  		var bestMove;
+  		if (player == 'O') {
+    		var bestScore = -10000;
+    		for (var i = 0; i < moves.length; i++) {
+      			if (moves[i].score > bestScore) {
+        			bestScore = moves[i].score;
+        			bestMove = i;
+      			}
+    		}
+  		} else {
+    		var bestScore = 10000;
+    		for (var i = 0; i < moves.length; i++) {
+      			if (moves[i].score < bestScore) {
+        			bestScore = moves[i].score;
+        			bestMove = i;
+      			}
+    		}
+  		}
+  		// console.log('moves[bestMove]', moves[bestMove])
+  		return moves[bestMove];
 	}
 
 	$('button').on('click', function(e){
